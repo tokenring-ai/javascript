58ea50325f77d5fd5aa598a641a67c321857dacb
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-23 14:24:46 -0700

    Converted the packageInfo export to a default export

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
 import packageJSON from './package.json' with {type: 'json'};
 import * as tools from "./tools.ts";
 
-export const packageInfo: TokenRingPackage = {
+export default {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
   install(agentTeam: AgentTeam) {
-    agentTeam.addTools(packageInfo, tools)
+    agentTeam.addTools(packageJSON.name, tools)
   }
-};
+} as TokenRingPackage;
 
